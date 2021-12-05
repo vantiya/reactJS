4d614d7df93b5a97493c31a40c8e5524e990c137
vantiya <parvezvantiya@gmail.com>
2021-12-05 23:24:00 +0530

    Make more components

--- avid-app/src/App.js
+++ avid-app/src/App.js
@@ -5,12 +5,41 @@ import ExpenseItem from './components/expenseItem';
 
 
 function App() {
+  const expenses = [
+    {
+      id: '1',
+      title: 'Buy Python Basic Course',
+      date: new Date(2021, 0, 1),
+      price: 4.49
+    },
+    {
+      id: '2',
+      title: 'Buy a Gift for my kiddo',
+      date: new Date(2021, 8, 3),
+      price: 50.49
+    },
+    {
+      id: '3',
+      title: 'Buy brand new TV',
+      date: new Date(2021, 7, 10),
+      price: 445.49
+    },
+    {
+      id: '4',
+      title: 'Buy myself MacBook Pro 16 Inch',
+      date: new Date(2021, 11, 10),
+      price: 1445.49
+    }
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p> Let's Get Started with Basic Custom Component.</p>
-        <ExpenseItem></ExpenseItem>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> Let's Get Started with Basic Custom Component.</p>        
+        <ExpenseItem title={expenses[0].title} price={expenses[0].price} date={expenses[0].date} />
+        <ExpenseItem title={expenses[1].title} price={expenses[1].price} date={expenses[1].date} />
+        <ExpenseItem title={expenses[2].title} price={expenses[2].price} date={expenses[2].date} />
+        <ExpenseItem title={expenses[3].title} price={expenses[3].price} date={expenses[3].date} />
       </header>
     </div>
   );
--- avid-app/src/components/expenseItem.js
+++ avid-app/src/components/expenseItem.js
@@ -1,16 +1,14 @@
 // eslint-disable-next-line
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 11, 31);
-    const expenseTitle = "Car Insurance";
-    const expensePrice = 259.9
+function ExpenseItem(props) {
     return (
-      <div className="expense-item">
-           <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item">
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                 <h2>{expenseTitle}</h2>
-                  <div className="expense-item__price">${expensePrice}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{"$"}{props.price}</div>
             </div>
         </div>
     );
