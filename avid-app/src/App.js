ff825528f81b6a11490021018c4a654d6d7e1575
vantiya <parvezvantiya@gmail.com>
2021-12-07 10:00:04 +0530

    Destructured code to make more component

--- avid-app/src/App.js
+++ avid-app/src/App.js
@@ -1,8 +1,7 @@
 // eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
-import ExpenseItem from './components/expenseItem';
-
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -34,12 +33,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> Let's Get Started with Basic Custom Component.</p>        
-        <ExpenseItem title={expenses[0].title} price={expenses[0].price} date={expenses[0].date} />
-        <ExpenseItem title={expenses[1].title} price={expenses[1].price} date={expenses[1].date} />
-        <ExpenseItem title={expenses[2].title} price={expenses[2].price} date={expenses[2].date} />
-        <ExpenseItem title={expenses[3].title} price={expenses[3].price} date={expenses[3].date} />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p> Let's Get Started with Basic Custom Component.</p>
+        <Expenses expenses={expenses} />
       </header>
     </div>
   );
