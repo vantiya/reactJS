ebebf8a568d5608db4629fe63f6dd20b6d16f228
vantiya <parvezvantiya@gmail.com>
2021-12-08 21:52:11 +0530

    Added custom component - Composition: Children Props

--- avid-app/src/components/Expenses/Expenses.js
+++ avid-app/src/components/Expenses/Expenses.js
@@ -1,14 +1,15 @@
 import ExpenseItem from './expenseItem';
+import Card from './../UI/Card';
 import './Expenses.css';
 
 function Expenses (props) {
     return(
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem title={props.expenses[0].title} price={props.expenses[0].price} date={props.expenses[0].date} />
             <ExpenseItem title={props.expenses[1].title} price={props.expenses[1].price} date={props.expenses[1].date} />
             <ExpenseItem title={props.expenses[2].title} price={props.expenses[2].price} date={props.expenses[2].date} />
             <ExpenseItem title={props.expenses[3].title} price={props.expenses[3].price} date={props.expenses[3].date} />
-        </div>
+        </Card>
     )
 }
 
