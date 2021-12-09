165cfc37b1ccce5a3d713bcd7fd4172dc203b176
vantiya <parvezvantiya@gmail.com>
2021-12-09 21:03:45 +0530

    playing with events

--- avid-app/src/components/Expenses/expenseItem.js
+++ avid-app/src/components/Expenses/expenseItem.js
@@ -4,13 +4,19 @@ import ExpenseDate from './ExpenseDate';
 import Card from './../UI/Card';
 
 function ExpenseItem(props) {
+    let title = props.title;
+    const btnClickHandler = () => {
+        title = "Updated"
+        console.log("CLicked");
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{"$"}{props.price}</div>
             </div>
+            <button onClick={btnClickHandler}>Update Title</button>
         </Card>
     );
 }
