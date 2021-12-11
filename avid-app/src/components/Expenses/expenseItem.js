a1f611dff5c0505153f4557400885902d5534443
vantiya <parvezvantiya@gmail.com>
2021-12-11 20:43:57 +0530

    playing with events - event State

--- avid-app/src/components/Expenses/expenseItem.js
+++ avid-app/src/components/Expenses/expenseItem.js
@@ -1,12 +1,13 @@
 // eslint-disable-next-line
-import './ExpenseItem.css';
+import React, {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from './../UI/Card';
+import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    let title = props.title;
+    const [title, setTitle] = useState(props.title);
     const btnClickHandler = () => {
-        title = "Updated"
+        setTitle("Updated")
         console.log("CLicked");
     }
     return (
