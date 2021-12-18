250dfa56955ea26e959c70bcbe93f09003733f64
vantiya <parvezvantiya@gmail.com>
2021-12-18 11:55:27 +0530

    Sytled componenet - Media Query

--- styling-react-components/src/components/UI/Button/Button.js
+++ styling-react-components/src/components/UI/Button/Button.js
@@ -11,6 +11,11 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
+  
   &:focus {
     outline: none;
   }
