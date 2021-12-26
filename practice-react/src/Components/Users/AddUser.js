3519262c79300d79e44f689ae21b4cee70330873
vantiya <parvezvantiya@gmail.com>
2021-12-26 12:19:40 +0530

    'Div Soup' & Handler as solution

--- practice-react/src/Components/Users/AddUser.js
+++ practice-react/src/Components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = props => {
 
@@ -45,7 +46,7 @@ const AddUser = props => {
     }
 
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
@@ -56,7 +57,7 @@ const AddUser = props => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     );
 };
 
